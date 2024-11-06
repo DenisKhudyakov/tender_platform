128dbb89653c2b9ab7ec45b2ff2e7421c93fe4e7
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-11-06 22:19:32 +0500

    create user model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 class User(AbstractUser):
-    pass
+    """
+    Класс пользователя, пользователь может быть поставщиком или сотрудником
+    """
+    username = None
+    email = models.EmailField(unique=True)
+    company_name = models.CharField(max_length=255, **NULLABLE)
+    phone = models.CharField(max_length=20, **NULLABLE)
+    inn = models.CharField(max_length=12, **NULLABLE)
+    is_employer = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
